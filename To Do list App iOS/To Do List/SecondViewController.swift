34d6f38b7483fe342aa7224e82213d6de927d4f5
Shadman Aziz <97sdmn@gmail.com>
2018-08-25 01:22:58 -0400

    Add files via upload
    Uploaded all the files under the project

---
+++ To Do list App iOS/To Do List/SecondViewController.swift
@@ -0,0 +1,67 @@
+
+//  To Do List
+//
+//  Created by Shadman Aziz on 25 August 2018.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet var itemTextField: UITextField!
+    
+    
+    @IBAction func add(_ sender: AnyObject) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(itemTextField.text!)
+            
+            print(items)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = ""
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
